465e1659119c0314f164bed4dc560ecc860ae61a
Varshil-Shah <varshilashah09@gmail.com>
2023-02-05 23:15:45 +0530

    :sparkles: Implement searching functionality & modify styling

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,20 +1,58 @@
-import React from "react";
+import React, { useContext, useState } from "react";
+import { collection, query, where, getDocs } from "firebase/firestore";
+import { db } from "../firebase";
+import { AuthContext } from "../context/AuthContext";
 
 const Search = () => {
+  const [username, setUsername] = useState("");
+  const [user, setUser] = useState(null);
+  const [error, setError] = useState("");
+
+  const { currentUser } = useContext(AuthContext);
+
+  const handleSearch = async () => {
+    if (currentUser.displayName === username.toLowerCase())
+      return setError("Cannot search your own name!!");
+
+    const q = query(
+      collection(db, "users"),
+      where("displayName", "==", username)
+    );
+
+    try {
+      const querySnapshot = await getDocs(q);
+      querySnapshot.forEach((doc) => {
+        setUser(doc.data());
+      });
+    } catch (error) {
+      console.log(error);
+      setError(error.message);
+    }
+  };
+
+  const handleKey = (e) => {
+    if (e.code === "Enter") handleSearch();
+  };
+
   return (
     <div className="search">
       <div className="searchForm">
-        <input type="text" placeholder="Search user..." />
-      </div>
-      <div className="userChat">
-        <img
-          src="https://images.unsplash.com/photo-1570295999919-56ceb5ecca61"
-          alt="user-img"
+        <input
+          type="text"
+          placeholder="Search user..."
+          onChange={(e) => setUsername(e.target.value)}
+          onKeyDown={handleKey}
         />
-        <div className="userChatInfo">
-          <span>Jane</span>
-        </div>
       </div>
+      {error && <p className="error-msg">{error}</p>}
+      {user && (
+        <div className="userChat">
+          <img src={user.photoURL} alt="user-img" />
+          <div className="userChatInfo">
+            <span>{user.displayName}</span>
+          </div>
+        </div>
+      )}
     </div>
   );
 };
