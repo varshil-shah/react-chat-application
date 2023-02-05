5d3209f56c0dca1b38ae09af1fbf32e98c79935d
Varshil-Shah <varshilashah09@gmail.com>
2023-02-05 22:14:12 +0530

    :passport_control: Implement signup and signin feature

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,17 +1,53 @@
-import React from "react";
+import React, { useState } from "react";
+import { auth } from "../firebase";
+import { signInWithEmailAndPassword } from "firebase/auth";
+import { useNavigate, Link } from "react-router-dom";
+import getFirebaseError from "../utils/firebase-error-handling";
 
 function Login() {
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState("");
+  const navigate = useNavigate();
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    setError("");
+
+    const email = e.target[0].value.trim();
+    const password = e.target[1].value.trim();
+
+    if (!email || !password) setError("Please fill out all the input fields!");
+
+    try {
+      setIsLoading(true);
+      await signInWithEmailAndPassword(auth, email, password);
+      navigate("/");
+    } catch (error) {
+      const errorMsg = getFirebaseError(error.code);
+      setError(errorMsg);
+    }
+
+    e.target[0].value = "";
+    e.target[1].value = "";
+    setIsLoading(false);
+  };
+
   return (
     <div className="formContainer">
       <div className="formWrapper">
         <span className="logo">संदेश</span>
         <span className="title">Login</span>
-        <form action="">
+        <form onSubmit={handleSubmit}>
           <input required type="email" placeholder="Email address" />
           <input required type="password" placeholder="Password" />
-          <button>Sign in</button>
+          <button disabled={isLoading}>
+            {isLoading ? "Loading..." : "Sign In"}
+          </button>
+          {error && <p className="error">{error}</p>}
         </form>
-        <p>Don't have an account? Signup</p>
+        <p>
+          Don't have an account? <Link to="/register">Sign Up</Link>
+        </p>
       </div>
     </div>
   );
--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,13 +1,74 @@
-import React from "react";
+import React, { useState } from "react";
+import { createUserWithEmailAndPassword, updateProfile } from "firebase/auth";
+import { auth, storage, db } from "../firebase";
+import { ref, uploadBytesResumable, getDownloadURL } from "firebase/storage";
+import { doc, setDoc } from "firebase/firestore";
+import { useNavigate, Link } from "react-router-dom";
+import getFirebaseError from "../utils/firebase-error-handling";
+
 import Add from "../img/addAvatar.png";
 
 function Register() {
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState("");
+  const navigate = useNavigate();
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    setError("");
+
+    const displayName = e.target[0].value.trim();
+    const email = e.target[1].value.trim();
+    const password = e.target[2].value.trim();
+    const file = e.target[3].files[0];
+
+    if (!displayName || !email || !password)
+      return setError("Please fill out all the input fields!");
+
+    if (!file) return setError("Please upload your profile picture!");
+
+    try {
+      setIsLoading(true);
+      const res = await createUserWithEmailAndPassword(auth, email, password);
+
+      const date = new Date().getTime();
+      const storageRef = ref(storage, `${displayName}-${date}`.toLowerCase());
+
+      await uploadBytesResumable(storageRef, file).then(() => {
+        getDownloadURL(storageRef).then(async (downloadURL) => {
+          await updateProfile(res.user, {
+            displayName,
+            photoURL: downloadURL,
+          });
+          await setDoc(doc(db, "users", res.user.uid), {
+            uid: res.user.uid,
+            displayName,
+            email,
+            photoURL: downloadURL,
+          });
+          await setDoc(doc(db, "userChats", res.user.uid), {});
+          navigate("/");
+        });
+      });
+    } catch (error) {
+      const errorMsg = getFirebaseError(error.code);
+      setError(errorMsg);
+    }
+
+    // reset values
+    e.target[0].value = "";
+    e.target[1].value = "";
+    e.target[2].value = "";
+    e.target[3].files = null;
+    setIsLoading(false);
+  };
+
   return (
     <div className="formContainer">
       <div className="formWrapper">
         <span className="logo">संदेश</span>
         <span className="title">Register</span>
-        <form action="">
+        <form onSubmit={handleSubmit}>
           <input required type="text" placeholder="Display name" />
           <input required type="email" placeholder="Email address" />
           <input required type="password" placeholder="Strong password" />
@@ -16,9 +77,14 @@ function Register() {
             <img src={Add} alt="file-picker" />
             <span>Add an avatar</span>
           </label>
-          <button>Sign up</button>
+          <button disabled={isLoading}>
+            {isLoading ? "Loading..." : "Sign Up"}
+          </button>
+          {error && <p className="error">{error}</p>}
         </form>
-        <p>Already have an account? Login</p>
+        <p>
+          Already have an account? <Link to="/register">Sign In</Link>
+        </p>
       </div>
     </div>
   );
