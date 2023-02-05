d06d9357d45e9b3c374159c493360cb05d487bd6
Varshil-Shah <varshilashah09@gmail.com>
2023-02-05 22:21:15 +0530

    :art: Design authContext to access auth anywhere in project

--- src/App.js
+++ src/App.js
@@ -1,11 +1,41 @@
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
+import { useContext } from "react";
+import { AuthContext } from "./context/AuthContext";
+
+import Register from "./pages/Register";
+import Login from "./pages/Login";
 import Home from "./pages/Home";
+
 import "./style.scss";
 
 function App() {
+  const { currentUser } = useContext(AuthContext);
+
+  const ProtectRoute = ({ children }) => {
+    if (!currentUser) {
+      return <Navigate to="/login" />;
+    }
+
+    return children;
+  };
+
   return (
-    <div className="App">
-      <Home />
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/">
+          <Route
+            index
+            element={
+              <ProtectRoute>
+                <Home />
+              </ProtectRoute>
+            }
+          ></Route>
+          <Route path="login" element={<Login />}></Route>
+          <Route path="register" element={<Register />}></Route>
+        </Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
