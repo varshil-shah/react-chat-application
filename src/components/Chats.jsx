c49d6732df115df8e9ba576cec91f75483e1aa1b
Varshil-Shah <varshilashah09@gmail.com>
2023-02-05 10:36:06 +0530

    :sparkles: Design a chats component to render all the users

---
+++ src/components/Chats.jsx
@@ -0,0 +1,40 @@
+import React from "react";
+
+const Chats = () => {
+  return (
+    <div className="chats">
+      <div className="userChat">
+        <img
+          src="https://images.unsplash.com/photo-1570295999919-56ceb5ecca61"
+          alt="user-img"
+        />
+        <div className="userChatInfo">
+          <span>Jane</span>
+          <p>Hello World</p>
+        </div>
+      </div>
+      <div className="userChat">
+        <img
+          src="https://images.unsplash.com/photo-1570295999919-56ceb5ecca61"
+          alt="user-img"
+        />
+        <div className="userChatInfo">
+          <span>Jane</span>
+          <p>Hello World</p>
+        </div>
+      </div>
+      <div className="userChat">
+        <img
+          src="https://images.unsplash.com/photo-1570295999919-56ceb5ecca61"
+          alt="user-img"
+        />
+        <div className="userChatInfo">
+          <span>Jane</span>
+          <p>Hello World</p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Chats;
