8c7217200c8b6a90da120c28758f939922926742
Varshil-Shah <varshilashah09@gmail.com>
2023-02-06 18:14:06 +0530

    :building_construction: Implement a func to capitalize first letter

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,6 +2,7 @@ import React, { useContext } from "react";
 import { signOut } from "firebase/auth";
 import { auth } from "../firebase";
 import { AuthContext } from "../context/AuthContext";
+import { capitalizeFirstLetter } from "../utils/utils";
 
 const Navbar = () => {
   const { currentUser } = useContext(AuthContext);
@@ -11,7 +12,7 @@ const Navbar = () => {
       <span className="logo">संदेश</span>
       <div className="user">
         <img src={currentUser.photoURL} alt="user-pic" />
-        <span>{currentUser.displayName}</span>
+        <span>{capitalizeFirstLetter(currentUser.displayName)}</span>
         <button onClick={() => signOut(auth)}>Logout</button>
       </div>
     </div>
